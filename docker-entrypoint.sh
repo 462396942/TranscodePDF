3ddac8713a69fc1475da10e1abf89316561bcbf9
slzcc <383821949@qq.com>
2018-08-31 12:15:27 +0800

    edit Makefile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ done
 
 python3 manage.py migrate
 
-sed -i "s/'%s\*\=%s' % (name,\ value)/'%s\="%s"'\ % (name,\ value.encode('utf-8'))/" /usr/local/lib/python3.7/site-packages/urllib3/fields.py
+sed -i "s/'%s\*\=%s' % (name,\ value)/'%s\="%s"'\ % (name,\ value.encode('utf-8'))/" /usr/local/lib/python3.5/site-packages/urllib3/fields.py
 
 exec uwsgi --socket 0.0.0.0:18089 \
       --chdir /TranscodePDF/ \
