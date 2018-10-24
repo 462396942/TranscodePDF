37e8158fafe6245e06059ebb944be13b51d5afcf
slzcc <383821949@qq.com>
2018-10-24 13:52:43 +0800

    edit Small mistake

--- ConvertFileFormat/controller/pdf.py
+++ ConvertFileFormat/controller/pdf.py
@@ -98,11 +98,11 @@ def _TranscodePDF(url, md5Str, sourceFile, filePath=None):
 	# 生成 PDF
 	if filePath:
 		response = FileToPDF(sourceFile, targetFilePath, fileCoding, os.path.join(filePath))
-		print(response)
+		print(response,sourceFile, targetFilePath, fileCoding, os.path.join(sourceFileSubPath))
 		ret = json.loads(response)
 	else:
 		response = FileToPDF(sourceFile, targetFilePath, fileCoding, os.path.join(sourceFileSubPath))
-		print(response)
+		print(response,sourceFile, targetFilePath, fileCoding, os.path.join(sourceFileSubPath))
 		ret = json.loads(response)
 
 	# 写入数据库
