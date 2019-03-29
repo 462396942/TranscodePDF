8c9b93ed93cddc18159265fc2c8533d27af09a88
slzcc <383821949@qq.com>
2019-03-29 17:43:58 +0800

    edit docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 set -e
 
-# https://docs.docker.com/compose/startup-order/
-host=$MYSQL_HOST
-MYSQL_ROOT_USER=$MYSQL_ROOT_USER
-MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
-
-until MYSQL_ROOT_USER=$MYSQL_ROOT_USER MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -h "$host" -e 'show databases;'; do
-  >&2 echo "MySQL is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "MySQL is up - executing command"
-
-
-isDatabaseTable=`mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -h "$host" -e "show databases;" | egrep "^$MYSQL_DATABASE$" | wc -l`
-
-if [[ $isDatabaseTable -eq 0 ]]; then 
-  echo "NOT Database, Perform create!"
-  mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -h "$host" -e "create database $MYSQL_DATABASE character set 'UTF8'"
-  mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -h "$host" -e "grant all on $MYSQL_DATABASE.* to $MYSQL_USER@'%' identified by '$MYSQL_PASSWORD'"
-fi
-
-python3 manage.py makemigrations
-
-python3 manage.py migrate
-
-sed -i "s/'%s\*\=%s' % (name,\ value)/'%s\="%s"'\ % (name,\ value.encode('utf-8'))/" /usr/local/lib/python3.5/dist-packages/urllib3/fields.py
-
 exec uwsgi --socket 0.0.0.0:18089 \
       --chdir /TranscodePDF/ \
       --env DJANGO_SETTINGS_MODULE=TranscodePDF.settings \
